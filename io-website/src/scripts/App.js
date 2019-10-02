43d62eea99687e6c51bde3484ba22d1cd74021ef
Robert Spier <robert.spier@gmail.com>
2019-10-02 10:53:08 +0200

    Revamp boilerplate to suit needs

---
+++ io-website/src/scripts/App.js
@@ -0,0 +1,18 @@
+// Core
+import React, { Component } from 'react';
+
+class App extends Component {
+	constructor() {
+		super();
+	}
+
+	render() {
+		return (
+			<div className="l-app">
+				<p>Hello world!</p>
+			</div>
+		);
+	}
+}
+
+export default App;
